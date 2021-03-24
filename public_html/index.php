54ac5f7c60fdd76ea1fdc2d573f39ea8fdd89043
Sagleft <sagleft@inbox.ru>
2021-03-24 16:26:07 +0400

    filter channel id

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,23 @@
 <?php
 	session_start();
-	require_once __DIR__ . "/../vendor/autoload.php";
+	require_once __DIR__ . '/../vendor/autoload.php';
 
 	$handler = new App\Handler();
+
+	$channelID  = $handler->dataFilter($_GET['cid']);
+	$actionType = $handler->dataFilter($_GET['type']);
+
+	if($channelID == '') {
+		$handler->user->redirect('/error/1001');
+	}
+	//TODO: check channelID with base64
+	switch($actionType) {
+		default:
+			$handler->user->redirect('/error/1000');
+		case 'join':
+			break;
+	}
+
 	$handler->render([
 		'tag'   => 'home',
 		'title' => 'CryptonClick',
